18ebe5cbc3a2a6626fc5b6f387ef43c862c84efc
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-05-21 12:11:03 +0900

    adminのアセット設定後AssetNotFound

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'pry-byebug'
 gem 'pry-doc'
 gem 'pry-rails'
 gem 'devise'
+gem 'activeadmin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-Item.create(name: "Bora one", price: 240000, user_id: 1)
-Item.create(name: "DT SWISS", price: 270000, user_id: 1)
-Item.create(name: "ROVAL", price: 300000, user_id: 1)
+Item.create!(name: "Bora one", price: 240000, user_id: 1)
+Item.create!(name: "DT SWISS", price: 270000, user_id: 1)
+Item.create!(name: "ROVAL", price: 300000, user_id: 1)
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
