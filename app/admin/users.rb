0239196cd19f56bc99aac3333ec8b3b1ae14b22d
hiroaki matsuoka <hiroakimatsuoka@MacBook-Pro.local>
2020-05-22 13:14:01 +0900

    ユーザー作成フォーム記述

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register User do
-  permit_params :user_name, :age, :address
+  permit_params :user_name, :age, :address, :email, :password
+
+  form do |f|
+    f.inputs do
+      f.input :user_name
+      f.input :email
+      f.input :age
+      f.input :password
+      f.input :password_confirmation
+    end
+    f.actions
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :users, only: :show
+  resources :users
   # root to: 'items#index'
   root to: 'home#top'
   resources :items
